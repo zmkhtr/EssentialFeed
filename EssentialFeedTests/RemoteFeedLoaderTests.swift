d73069339d392a179cb8790b28e0d4a23876068d
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2021-12-19 17:57:57 +0700

    Make the HTTPClient a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,19 +8,24 @@
 import XCTest
 
 class RemoteFeedLoader {
+    
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load(){
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    
-    func get(from url : URL) {}
+protocol HTTPClient {
+    func get(from url : URL)
 }
 
 class HTTPClientSpy: HTTPClient {
-    override func get(from url : URL){
+    func get(from url : URL){
         requestedURL = url
     }
     
@@ -31,8 +36,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         
         XCTAssertNil(client.requestedURL)
@@ -40,8 +44,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_requestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
