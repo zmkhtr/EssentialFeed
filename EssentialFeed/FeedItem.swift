c8125e9a3474866b5f981b22742866fb04415145
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2021-12-19 17:28:18 +0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  Feed.swift
+//  EssentialFeed
+//
+//  Created by PT.Koanba on 19/12/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
