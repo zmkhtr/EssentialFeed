fd1ce316c54611e5c4b88241c002ab3692f595a1
zmkhtr <azammukhtar3@gmail.com>
2022-01-16 21:24:51 +0700

    Add `LocalFeedItem` data transfer representation to decouple storage framework from `FeedItem` data models

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -12,5 +12,19 @@ public protocol FeedStore {
     typealias InsertionCompletions = (Error?) -> Void
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletions)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletions)
+}
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
