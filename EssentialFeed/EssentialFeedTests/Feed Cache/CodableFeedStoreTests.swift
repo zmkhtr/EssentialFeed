0b522230fe5c855cf5cf11ae540a9ce86ce77434
zmkhtr <azammukhtar3@gmail.com>
2022-02-10 21:09:38 +0700

    Extract system under test (sut) creation into a factory method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -75,7 +75,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut  = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache retieval")
         
         sut.retrieve { result in
@@ -93,7 +93,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut  = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache retieval")
         
         sut.retrieve { firstResult in
@@ -113,7 +113,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-        let sut  = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         let exp = expectation(description: "Wait for cache retieval")
@@ -135,5 +135,11 @@ class CodableFeedStoreTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    // - MARK: Helpers
+    
+    private func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
 }
