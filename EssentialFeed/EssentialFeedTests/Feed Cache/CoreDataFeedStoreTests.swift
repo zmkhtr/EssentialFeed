efd279e0ae949742aa8ab99cf121a3c9feeb6bf8
zmkhtr <azammukhtar3@gmail.com>
2022-02-14 19:23:50 +0700

    `CoreDataFeedStore.retrieve()` has no side-effects on non-empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -29,7 +29,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
